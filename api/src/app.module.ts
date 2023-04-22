d6ac681e2636616d81a854579772302ded546b31
Artiom Karimov <lunatic0259@gmail.com>
2023-04-22 14:09:05 +0300

    added initial resources

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { OperationsModule } from './operations/operations.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, AccountsModule, OperationsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
