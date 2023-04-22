6d9951fe39c6fac0f0c56035a0d1114837fec85d
Artiom Karimov <lunatic0259@gmail.com>
2023-04-22 13:41:57 +0300

    added config module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
+
+  logger.log('Creating app...');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const port = config.get('PORT') || 3000;
+  await app.listen(port);
+  logger.log(`App is listenning on port ${port}`);
 }
 bootstrap();
