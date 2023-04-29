26a53fd77e9b2198132d86dc52ff9670015c8a12
Artiom Karimov <lunatic0259@gmail.com>
2023-04-29 21:31:26 +0300

    added simple typeorm config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,10 +7,21 @@ import { AccountsModule } from './accounts/accounts.module';
 import { OperationsModule } from './operations/operations.module';
 import { AuthModule } from './auth/auth.module';
 import { OperationCategoriesModule } from './operation-categories/operation-categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
+      entities: [],
+      synchronize: true,
+    }),
     UsersModule,
     AccountsModule,
     OperationsModule,
